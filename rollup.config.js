9261bacfb5efb5a1eacce87689bc3b9fe63af830
Li <1977735808@qq.com>
2023-01-06 17:47:55 +0800

    :wrench: update the configuration and allow packaging into esm, cjs and types

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,36 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 
-export default {
+const outputOptions = {
+  es: './lib/esm',
+  cjs: './lib/node',
+};
+
+const plugins = [typescript(), resolve()];
+
+const configs = Object.keys(outputOptions).map((format) => {
+  const dir = outputOptions[format];
+
+  const output = {
+    format,
+    sourcemap: true,
+    // dir,
+    file: format === 'cjs' ? dir + '/index.cjs' : dir + '/index.js'
+  };
+
+  return {
+    input: './src/index.ts',
+    output,
+    plugins,
+  };
+});
+
+export default configs.concat({
   input: './src/index.ts',
   output: {
-    format: 'cjs',
-    sourcemap: true,
-    dir: './lib'
+    dir: 'lib',
+    format: 'es',
   },
-  plugins: [typescript(), resolve()]
-}
+  plugins: [...plugins, dts()],
+});
