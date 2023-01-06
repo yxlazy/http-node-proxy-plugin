267e589361d3a239ed57bd46387bc7694b48a95a
Li <1977735808@qq.com>
2023-01-06 11:24:00 +0800

    :wrench: update rollup config & tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+
 export default {
   input: './src/index.ts',
   output: {
     format: 'cjs',
     sourcemap: true,
     dir: './lib'
-  }
+  },
+  plugins: [typescript(), resolve()]
 }
